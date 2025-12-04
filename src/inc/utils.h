5f50cb6a5c0e51f6b4a11e27cd508f26b9fbfef6
oarixsapkota <oarixsapkota@gmail.com>
2025-12-04 16:12:40 +0545

    refactor

---
+++ src/inc/utils.h
@@ -0,0 +1,6 @@
+#ifndef VALT_UTILS
+#define VALT_UTILS
+
+char *read_file(const char *file_name);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,41 +2,7 @@
 #include <stdlib.h>
 
 #include "inc/lexer.h"
-
-char *read_file(const char *file_name) {
-  // open file.
-  FILE *file_ptr = fopen(file_name, "rb");
-  if (!file_ptr) {
-    fprintf(stderr, "Error opening file %s\n", file_name);
-    return NULL;
-  }
-  // seek to end to get file size.
-  if (fseek(file_ptr, 0, SEEK_END) != 0) {
-    fprintf(stderr, "Error seeking end of file %s\n", file_name);
-    fclose(file_ptr);
-    return NULL;
-  }
-  long file_size = ftell(file_ptr);
-  // check if file size is correct.
-  if (file_size < 0) {
-    fclose(file_ptr);
-    fprintf(stderr, "Error reading file %s\n", file_name);
-    return NULL;
-  }
-  rewind(file_ptr);
-  // allocate memory for the buffer.
-  char *buffer = (char *)malloc(file_size + 1);
-  if (!buffer) {
-    fclose(file_ptr);
-    fprintf(stderr, "Error allocating memory\n");
-    return NULL;
-  }
-  // get the file into buffer.
-  size_t file_length = fread(buffer, 1, (size_t)file_size, file_ptr);
-  fclose(file_ptr);
-  buffer[file_length] = '\0';
-  return buffer;
-}
+#include "inc/utils.h"
 
 void print_tokens(Token *token) {
   while (token) {
